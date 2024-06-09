849fe0d7c9b696e3e56070e894bd703031539936
anushika0167 <152178265+anushika0167@users.noreply.github.com>
2024-06-09 04:59:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability shpuld be higher
+   
+  
+
+let name = "hitesh"
+let age= 18
+let isLoggedIn = false
+let state;
+
+
+// primitive datatypes
+// number=> 2 to pwer 53 is its range 
+// bigint
+// string=>""
+// boolean => true/ false 
+// null => standalone value 
+// undefined => 
+// symbol=> uniqueness
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
