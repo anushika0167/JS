6f52361c15ee57ff56df8f7ea138c7c27f378ea9
anushika0167 <152178265+anushika0167@users.noreply.github.com>
2024-06-16 04:17:11 +0000

    scopes

---
+++ 01_basics/11_scopes.js
@@ -0,0 +1,91 @@
+
+//var c = 300 
+let a = 300
+
+if (true) {
+   let a = 10
+   const b = 20 
+  //console.log("INNER", a);
+
+}
+ 
+
+//  for (let i = 0; i < array.length; i++) {
+//  const element = array i];
+    
+//  }
+
+//console.log (a);
+//console.log(b);
+//console.log(c);
+
+//document object model (dom)
+
+
+
+function one( ) {
+
+    const username = "hitesh"
+
+    function two(){
+
+        const website = "youtube"
+       // console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+}
+
+//one()
+
+
+
+
+
+
+
+if (true) {
+    const username = "hitesh"
+
+    if (username === "hitesh") {
+        const website = " youtube"
+//console.log(username + website);
+
+    }
+//console.log(website);
+
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++interesting++++++++++++++++++++++
+
+
+console.log(addone (5))
+function addone(num) {
+    return num+1 
+    
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num +2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
