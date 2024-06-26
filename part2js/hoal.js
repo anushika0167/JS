ec4c0edea481337cf4fe9b0314c91151f9764619
anushika0167 <152178265+anushika0167@users.noreply.github.com>
2024-06-26 05:57:01 +0000

    hoal

---
+++ part2js/hoal.js
@@ -0,0 +1,54 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb:'ruby',
+    swift:'swift by apple'
+
+}
+
+for (const key in myObject) {
+ // console.log(myObject[key]);
+}
+
+
+for (const key in myObject) {
+   // console.log(key);
+  }
+
+
+
+  for (const key in myObject) {
+    //console.log(`${key} short is for ${myObject[key]}`);
+  }
+
+
+
+
+
+const programming = ['js', 'rb', 'py', 'java']
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+
+
+//objects k uper forin loop and arrays k upr forof loop 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
